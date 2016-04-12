2675dff0ea4deda5871497a2a4c70606a05c6fcf
Tran Quoc Viet <tqv9680@outlook.com>
2016-04-12 11:04:38 +0700

    sss

---
+++ Reflector/Nsurvey_UserProvider/Votation.NSurvey.LDAPProvider/ILDAPProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Votations.NSurvey.Data;
+
+namespace Votation.NSurvey.LDAPProvider
+{
+    public interface ILDAPProvider
+    {
+        VoterADInfo GetVoterInfo(string userName);
+        IEnumerable<string> SearchAD(string filterPhase);
+        IEnumerable<string> SearchOU(string filterPhase);
+        IEnumerable<string> SearchCN(string filterPhase);
+
+        IEnumerable<string> GetOUPath(string path, bool getFull);
+        string GetFirstPath(string path, bool getFull);
+
+    }
+}
